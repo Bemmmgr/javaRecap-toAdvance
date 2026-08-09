215a59455302fd31af4ef4948ac2d724593003ef
Xiangyu Li <lix0230@gmail.com>
2026-08-09 23:28:36 +0200

    Add gaming console example and ignore course resources

--- 01-spring-core/src/main/java/com/steven/springcore/AppGamingBasicJava.java
+++ 01-spring-core/src/main/java/com/steven/springcore/AppGamingBasicJava.java
@@ -1,7 +1,12 @@
 package com.steven.springcore;
 
+import com.steven.springcore.game.GameRunner;
+import com.steven.springcore.game.MarioGame;
+
 public class AppGamingBasicJava {
     public static void main(String[] args) {
-
+        var game = new MarioGame();
+        var gameRunner = new GameRunner(game);
+        gameRunner.run();
     }
 }
---
+++ 01-spring-core/src/main/java/com/steven/springcore/game/GameRunner.java
@@ -0,0 +1,15 @@
+package com.steven.springcore.game;
+
+public class GameRunner {
+
+    private GamingConsole game;
+
+    public GameRunner(GamingConsole game) {
+        this.game = game;
+    }
+
+    public void run() {
+        System.out.println("Running game: " + game);
+        game.up();
+    }
+}
