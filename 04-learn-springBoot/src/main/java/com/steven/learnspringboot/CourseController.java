df155b09d0fd3b6330e4699fa97d0aba363ac9ef
Xiangyu Li <lix0230@gmail.com>
2026-08-15 11:50:26 +0200

    Initializer, build hello API, autoconfiguration, Devtools, profiles, properties and actuator, understanding section

---
+++ 04-learn-springBoot/src/main/java/com/steven/learnspringboot/CourseController.java
@@ -0,0 +1,20 @@
+package com.steven.learnspringboot;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses() {
+        return Arrays.asList(
+                new Course(1, "Learn AWS", "gus"),
+                new Course(2, "MicroService", "bemmgr"),
+                new Course(3, "Azure", "noname")
+        );
+    }
+}
